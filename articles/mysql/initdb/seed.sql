b2871c213458366f90a852a626d202f3f45d1aa0
Stjampot <naal0004@hz.nl>
2024-12-11 14:37:48 +0100

    finished recipe API

--- articles/mysql/initdb/seed.sql
+++ articles/mysql/initdb/seed.sql
@@ -1,7 +1,7 @@
 USE articles_app;
 
 INSERT INTO articles (name, imgUrl, info) VALUES
-('Understanding sdlkfjaSvelteKit', 'https://example.com/images/sveltekit.jpg', 'Learn the fundamentals of SvelteKit and how it simplifies building web applications.'),
+('bfsadlfkajs;fk', 'https://example.com/images/sveltekit.jpg', 'Learn the fundamentals of SvelteKit and how it simplifies building web applications.'),
 ('The Power of Reactive Programming', 'https://example.com/images/reactive-programming.jpg', 'Dive into reactive programming concepts and how they are implemented in Svelte.'),
 ('State Management in Svelte', 'https://example.com/images/state-management.jpg', 'Explore various approaches to manage state in Svelte applications.'),
 ('SvelteKit and SSR: A Match Made in Heaven', 'https://example.com/images/sveltekit-ssr.jpg', 'Discover the power of server-side rendering (SSR) in SvelteKit applications.'),
