32487c53695f72f919cad47f9c04d38e9eff72c1
Stjampot <naal0004@hz.nl>
2025-01-06 13:37:26 +0100

    leaf system backend finished

--- leafs/mysql/initdb/init.sql
+++ leafs/mysql/initdb/init.sql
@@ -1,22 +1,17 @@
-CREATE DATABASE IF NOT EXISTS leafAmount_app;
-USE leafAmount_app;
+CREATE DATABASE IF NOT EXISTS leafs_app;
+USE leafs_app;
 
 -- Create root user with password if it doesn't exist
 CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'rootpassword';
 
 -- Grant privileges to the root user
-GRANT ALL PRIVILEGES ON goals_app.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON leafs_app.* TO 'root'@'%';
 
 -- Ensure privileges are flushed
 FLUSH PRIVILEGES;
 
 -- Create table (if it doesn't exist already)
-CREATE TABLE IF NOT EXISTS goals (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    goal VARCHAR(255) NOT NULL,              
-    description TEXT NOT NULL,               
-    level ENUM('beginner', 'intermediate', 'advanced') NOT NULL, 
-    picked BOOLEAN DEFAULT FALSE,
-    last_picked DATETIME     
+CREATE TABLE IF NOT EXISTS leafAmount (
+    leafAmount INT NOT NULL
 );
 
