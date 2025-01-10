aeb129582acea7bfc0266f6d9f84553a245c34d4
Stjampot <naal0004@hz.nl>
2025-01-10 11:18:01 +0100

    fixes

--- users/mysql/initdb/seed.sql
+++ users/mysql/initdb/seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (username, totalemissions) VALUES 
-('TeriakiEnjoyer', 12),
-('IloveCooking123', 26),
-('GordonRamsay', 8);
+INSERT INTO users (username, totalemissions, leafamount, dietgoal) VALUES 
+('TeriakiEnjoyer', 12, 4, 'Flexitarian'),
+('IloveCooking123', 8, 8, 'Vegan'),
+('GordonRamsay', 18, 999, 'Keto');
